4942261feccbf63c3736f8c59cb580da92b230b3
moodyhamster <j04651934@gmail.com>
2026-09-04 23:06:16 +0100

    Keep KDE profile desktop-only

--- profiles/desktop-kde.nix
+++ profiles/desktop-kde.nix
@@ -1,12 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   # KDE Plasma profile for desktop systems.
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  users.users.jason.packages = with pkgs; [
-    kdePackages.kate
-  ];
 }
