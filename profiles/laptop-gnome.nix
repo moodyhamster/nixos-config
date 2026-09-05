70ce1227d0bce0f68a83fd01db308eb620aea6ae
moodyhamster <j04651934@gmail.com>
2026-09-05 11:55:39 +0100

    Enable desktop icons on GNOME laptops

--- profiles/laptop-gnome.nix
+++ profiles/laptop-gnome.nix
@@ -1,8 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # GNOME profile for laptops.
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  # GNOME itself does not show files/icons on the desktop. Install and enable
+  # GTK4 Desktop Icons NG (DING) so ~/Desktop works on GNOME laptops too.
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.gtk4-desktop-icons-ng-ding
+  ];
+
+  programs.dconf.enable = true;
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/shell" = {
+          enabled-extensions = [
+            pkgs.gnomeExtensions.gtk4-desktop-icons-ng-ding.extensionUuid
+          ];
+        };
+      };
+    }
+  ];
 }
