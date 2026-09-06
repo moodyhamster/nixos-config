e0b55320e8c196ea306b5a103aeeb2656825749e
moodyhamster <j04651934@gmail.com>
2026-09-06 22:41:30 +0100

    Add second Dell OptiPlex host base

---
+++ hosts/dell-optiplex-2/base.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+    ../../modules/common.nix
+    ../../profiles/gaming.nix
+    ../../profiles/hardware/dell-optiplex.nix
+  ];
+
+  networking.hostName = "dell-optiplex-2";
+
+  # This machine was first installed on NixOS 26.05.
+  system.stateVersion = "26.05";
+}
