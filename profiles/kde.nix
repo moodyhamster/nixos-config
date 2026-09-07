d3fe273bda98da86b7505517eaefed2158b2acbe
moodyhamster <j04651934@gmail.com>
2026-09-07 17:08:31 +0100

    Move Bluetooth ownership into KDE/desktop profile separation

--- profiles/kde.nix
+++ profiles/kde.nix
@@ -4,6 +4,7 @@
   # KDE Plasma desktop profile. Import this from a host's kde.nix so KDE is
   # the only desktop environment enabled in that system build.
   services.xserver.enable = true;
+  services.displayManager.gdm.enable = false;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
