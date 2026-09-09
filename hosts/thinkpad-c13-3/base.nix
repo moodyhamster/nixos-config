7b0ea881cd532db0e53ae6b27d013c7dba14a0af
moodyhamster <j04651934@gmail.com>
2026-09-09 19:04:40 +0100

    Add third ThinkPad C13 host

---
+++ hosts/thinkpad-c13-3/base.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+    ../../modules/common.nix
+    ../../profiles/laptop.nix
+    ../../profiles/hardware/thinkpad-c13.nix
+  ];
+
+  networking.hostName = "thinkpad-c13-3";
+
+  # Keep the value from this machine's original installation.
+  system.stateVersion = "26.05";
+}
