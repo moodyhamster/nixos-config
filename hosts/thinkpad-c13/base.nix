9f191500250af64404bd75c76852e5ec26eaa8f4
moodyhamster <j04651934@gmail.com>
2026-09-17 19:11:10 +0100

    Enable Squid proxy on ThinkPad C13

--- hosts/thinkpad-c13/base.nix
+++ hosts/thinkpad-c13/base.nix
@@ -10,6 +10,10 @@
 
   networking.hostName = "thinkpad-c13";
 
+  # Provide a LAN HTTP/HTTPS forward proxy for legacy devices such as the PS3.
+  services.squid.enable = true;
+  networking.firewall.allowedTCPPorts = [ 3128 ];
+
   # Keep the value from this machine's original installation.
   system.stateVersion = "26.05";
 }
